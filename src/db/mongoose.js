d85dc409f9a7c5167ab5a32da26fc88667542acd
Pratik Goti <88868354+Pratik8418@users.noreply.github.com>
2023-04-13 00:04:10 +0530

    useFindAndModify

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -3,7 +3,7 @@ const error = require('mongoose/lib/error');
 const { Number } = require('mongoose/lib/schema/index');
 
 
-mongoose.set('useFindAndModify', false);
+// mongoose.set('useFindAndModify', true);
 
 const connectionUrl = 'mongodb://127.0.0.1:27017/task-manager-api';
 
